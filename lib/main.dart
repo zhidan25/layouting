e91ccbd1f93c8d1dea703e2c598f910f718352e2
FlutLab.io <>
2022-05-24 08:47:38 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class BelajarAppBar extends StatelessWidget {
                       tabs: [
                         new Tab(icon: new Icon(Icons.audiotrack), text: "Songs"),
                         new Tab(icon: new Icon(Icons.collections), text: "Gallery"),
-                        new Tab(icon: new Icon(Icons.playlist), text: "Playlist"),
+                        new Tab(icon: new Icon(Icons.headset), text: "Headset"),
                       ],
                     ),
                   ]),

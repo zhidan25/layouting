cac48fc06cec2c61a16b8046fa4bfb368bc34133
FlutLab.io <>
2022-05-24 07:46:18 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,85 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MaterialApp(
-    home: new HalamanSatu(),
-  ));
+  runApp(MyApp());
 }
 
-class HalamanSatu extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BelajarAppBar(),
+    );
+  }
+}
+
+class BelajarAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[50],
-      appBar: new AppBar(
-        backgroundColor: Colors.blue[900],
-        leading: new Icon(Icons.home),
-        title: new Center(
-          child: new Text("Zhidan Ost"),
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                expandedHeight: 200.0,
+                floating: false,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Text("Belajar SliverAppBar",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      )),
+                  background: Image(
+                    image: AssetImage('assets/images/about-pic3.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SliverPadding(
+                padding: new EdgeInsets.all(10.0),
+                sliver: new SliverList(
+                  delegate: new SliverChildListDelegate([
+                    TabBar(
+                      labelColor: Colors.black87,
+                      unselectedLabelColor: Colors.grey,
+                      tabs: [
+                        new Tab(icon: new Icon(Icons.audiotrack), text: "Songs"),
+                        new Tab(icon: new Icon(Icons.collections), text: "Gallery"),
+                      ],
+                    ),
+                  ]),
+                ),
+              ),
+            ];
+          },
+          body: Center(
+            child: Text("belajar app bar"),
+          ),
         ),
-        actions: <Widget>[
-          new Icon(Icons.search)
-        ], // <Widget>[]
       ),
     );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+  final TabBar _tabBar;
+
+  double get minExtent => _tabBar.preferredSize.height;
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  Widget build(BuildContext context, double shrinkOffset, bool overlabsContents) {
+    return new Container(
+      child: _tabBar,
+    );
+  }
+
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
   }
 }
